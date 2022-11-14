4198bdaa79fe2dabe6039dc26fb0e12713322fa4
Justin <>
2022-11-14 17:46:49 +0100

    modify Serie

--- src/Entity/Serie.php
+++ src/Entity/Serie.php
@@ -16,6 +16,10 @@ class Serie
     #[ORM\Column(length: 255)]
     private ?string $name = null;
 
+    #[ORM\OneToOne(cascade: ['persist', 'remove'])]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?Type $type = null;
+
     public function getId(): ?int
     {
         return $this->id;
@@ -29,6 +33,18 @@ class Serie
     public function setName(string $name): self
     {
         $this->name = $name;
+
+        return $this;
+    }
+
+    public function getType(): ?Type
+    {
+        return $this->type;
+    }
+
+    public function setType(Type $type): self
+    {
+        $this->type = $type;
 
         return $this;
     }
