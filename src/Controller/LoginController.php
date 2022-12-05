cda7f93de1cc5d4632b5f429e6eb62ee84944f54
Hedi-ZAIR <hedi.marwan@hotmail.fr>
2022-12-05 19:50:09 +0100

    le login rend fou je comprends rien donc FF

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -2,7 +2,11 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Form\LoginFormType;
+use App\Form\RegistrationFormType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
@@ -15,12 +19,17 @@ class LoginController extends AbstractController
 
         //Le controleur ne sert qu'a rediriger sur la page de connexion.
 
+//        $user = new User();
+//        $form = $this->createForm(LoginFormType::class, $user);
+//        $form->handleRequest($request);
+
         $error = $authenticationUtils->getLastAuthenticationError();
         $lastUsername = $authenticationUtils->getLastUsername();
 
         return $this->render('login/index.html.twig', [
             'last_username' => $lastUsername,
             'error'         => $error,
+//            'loginForm' => $form->createView(),
         ]);
     }
 }
