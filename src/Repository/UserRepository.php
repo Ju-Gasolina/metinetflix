1751f2ccf1813600e93ce99bfe70c301efdfb863
Ju-Gasolina <justin.carra2002@gmail.com>
2022-12-05 09:42:48 +0100

    add form types

--- src/Repository/UserRepository.php
+++ src/Repository/UserRepository.php
@@ -2,26 +2,26 @@
 
 namespace App\Repository;
 
-use App\Entity\Editor;
+use App\Entity\User;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
 
 /**
- * @extends ServiceEntityRepository<Editor>
+ * @extends ServiceEntityRepository<User>
  *
- * @method Editor|null find($id, $lockMode = null, $lockVersion = null)
- * @method Editor|null findOneBy(array $criteria, array $orderBy = null)
- * @method Editor[]    findAll()
- * @method Editor[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ * @method User|null find($id, $lockMode = null, $lockVersion = null)
+ * @method User|null findOneBy(array $criteria, array $orderBy = null)
+ * @method User[]    findAll()
+ * @method User[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
  */
 class UserRepository extends ServiceEntityRepository
 {
     public function __construct(ManagerRegistry $registry)
     {
-        parent::__construct($registry, Editor::class);
+        parent::__construct($registry, User::class);
     }
 
-    public function save(Editor $entity, bool $flush = false): void
+    public function save(User $entity, bool $flush = false): void
     {
         $this->getEntityManager()->persist($entity);
 
@@ -30,7 +30,7 @@ class UserRepository extends ServiceEntityRepository
         }
     }
 
-    public function remove(Editor $entity, bool $flush = false): void
+    public function remove(User $entity, bool $flush = false): void
     {
         $this->getEntityManager()->remove($entity);
 
@@ -40,7 +40,7 @@ class UserRepository extends ServiceEntityRepository
     }
 
 //    /**
-//     * @return Editor[] Returns an array of Editor objects
+//     * @return User[] Returns an array of User objects
 //     */
 //    public function findByExampleField($value): array
 //    {
@@ -54,7 +54,7 @@ class UserRepository extends ServiceEntityRepository
 //        ;
 //    }
 
-//    public function findOneBySomeField($value): ?Editor
+//    public function findOneBySomeField($value): ?User
 //    {
 //        return $this->createQueryBuilder('e')
 //            ->andWhere('e.exampleField = :val')
