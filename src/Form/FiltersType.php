32429e103caced2481616686b8aabf3e488eaaaf
Hedi-ZAIR <hedi.marwan@hotmail.fr>
2022-12-12 18:25:19 +0100

    new branch

--- src/Form/FiltersType.php
+++ src/Form/FiltersType.php
@@ -2,13 +2,21 @@
 
 namespace App\Form;
 
+
+use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\DateIntervalType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\Extension\Core\Type\RadioType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TimeType;
 use Symfony\Component\Form\FormBuilderInterface;
 
+#[ORM\Entity]
+#[ORM\Table(name: 'filters_type')]
 class FiltersType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
@@ -32,7 +40,6 @@ class FiltersType extends AbstractType
 
                     ],
                     'choice_attr' => [
-
                         'Vote average ↑' => ['class' => 'btn-check'],
                         'Vote average ↓' => ['class' => 'btn-check'],
                         'Popularity ↑' => ['class' => 'btn-check'],
@@ -45,6 +52,31 @@ class FiltersType extends AbstractType
 
                 ]
             )
+            ->add( 'minDate', DateType::class, [
+
+                    'widget' => 'choice',
+                    'required' => 'false',
+                ]
+            )
+            ->add( 'maxDate', DateType::class, [
+
+                    'widget' => 'choice',
+                    'required' => 'false',
+                ]
+            )
+
+            ->add('maxTime', TimeType::class, [
+                'input'  => 'datetime',
+                'widget' => 'choice',
+                'required' => 'false',
+            ])
+            ->add('includeAdult', CheckboxType::class, [
+                'label'    => 'Show this entry publicly?',
+                'required' => false,
+                'attr' => [
+                    'class' => ''
+                ]
+            ])
             ->add('search', SubmitType::class, array('attr' => array('class' => 'button-basic-custom text-center')));
 
 
